95e16516a8160b928b085758a3a413d742dbd586
murprakoso <prakoso.clan04@gmail.com>
2021-01-02 15:41:44 +0700

    refactoring redux membuat folder tersendiri untuk redux(lebih rapi)

--- src/containers/pages/App/index.js
+++ src/containers/pages/App/index.js
@@ -3,34 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Dashboard from '../Dashboard';
 import Login from '../Login';
 import Register from '../Register';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-
-
-const initialState = {
-  popup: false,
-  isLogin: false
-}
-
-const reducer = (state = initialState, action) => {
-  if (action.type === 'CHANGE_POPUP') {
-    return {
-      ...state,
-      popop: action.value
-    }
-  }
-  if (action.type === 'CHANGE_ISLOGIN') {
-    return {
-      ...state,
-      isLogin: action.value
-    }
-  }
-
-  return state;
-}
-
-// const store = createStore(reducers, compose(applyMiddleware(thunk)));
-const store = createStore(reducer);
+import { store } from '../../../config/redux'
 
 
 /** Start */
